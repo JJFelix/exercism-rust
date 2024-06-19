bd3a645c091bcc9786539b5c7eb1ce64145111be
JJFelix <felixomwenga441@gmail.com>
2024-06-19 19:43:15 +0300

    Armstrong numbers

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    todo!("true if {num} is an armstrong number")
+	let num_str = num.to_string();
+	let  num_digits = num_str.len() as u32;
+    
+    let sum: u64 = num_str
+        .chars()
+        .map(|c| {
+            let digit = c.to_digit(10).unwrap() as u64;
+            digit.pow(num_digits)
+        })
+        .sum();
+        
+    sum == num as u64     
 }
